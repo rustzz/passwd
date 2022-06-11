7cf40097f83606e2c7991664409d1286512f542c
rustzz <rustzz.devel@gmail.com>
2022-06-11 05:48:40 +0500

    menu: service card navbar: menu button
    todo:
        fix: circle progress bar refreshing
        new: drawer with settings (optional)

--- lib/activities/add.dart
+++ lib/activities/add.dart
@@ -1,97 +1,108 @@
 import 'package:flutter/material.dart';
 
 class Add extends StatefulWidget {
-	const Add({Key? key}) : super(key: key);
+  const Add({Key? key}) : super(key: key);
 
-	@override
-	AddState createState() => AddState();
+  @override
+  AddState createState() => AddState();
 }
 
 class AddState extends State<Add> {
-	final nameTF = TextEditingController();
-	final usernameTF = TextEditingController();
-	final passwordTF = TextEditingController();
-	final otpTF = TextEditingController();
+  final nameTF = TextEditingController();
+  final usernameTF = TextEditingController();
+  final passwordTF = TextEditingController();
+  final otpTF = TextEditingController();
 
-	bool nameEmpty = false;
-	bool usernameEmpty = false;
-	bool passwordEmpty = false;
-	bool otpEmpty = false;
+  bool nameEmpty = false;
+  bool usernameEmpty = false;
+  bool passwordEmpty = false;
+  bool otpEmpty = false;
 
-	@override
-	void dispose() {
-		nameTF.dispose();
-		usernameTF.dispose();
-		passwordTF.dispose();
-		otpTF.dispose();
-		super.dispose();
-	}
+  @override
+  void dispose() {
+    nameTF.dispose();
+    usernameTF.dispose();
+    passwordTF.dispose();
+    otpTF.dispose();
+    super.dispose();
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				shadowColor: Colors.transparent,
-				title: const Text("passwd"),
-				centerTitle: true,
-			),
-			body: Padding(
-				padding: const EdgeInsets.all(10),
-				child: Column(
-					children: [
-						TextField(
-							autofocus: true,
-							controller: nameTF,
-							decoration: InputDecoration(
-								labelText: "Название сервиса",
-								errorText: nameEmpty ? "Впишите название" : null,
-							),
-						),
-						TextField(
-							controller: usernameTF,
-							decoration: InputDecoration(
-								labelText: "Имя пользователя",
-								errorText: usernameEmpty ? "Впишите имя пользователя" : null,
-							),
-						),
-						TextField(
-							controller: passwordTF,
-							decoration: InputDecoration(
-								labelText: "Пароль",
-								errorText: passwordEmpty ? "Впишите пароль" : null,
-							),
-						),
-						TextField(
-							controller: otpTF,
-							decoration: InputDecoration(
-								labelText: "OTP",
-								errorText: otpEmpty ? "Впишите ключ OTP" : null,
-							),
-						),
-						Container(
-							margin: const EdgeInsets.only(top: 50.0),
-							child: IconButton(
-								onPressed: () {
-									setState(() {
-					                  nameTF.text.replaceAll(" ", "").isEmpty ? nameEmpty = true : nameEmpty = false;
-					                  usernameTF.text.replaceAll(" ", "").isEmpty ? usernameEmpty = true : usernameEmpty = false;
-					                  passwordTF.text.replaceAll(" ", "").isEmpty ? passwordEmpty = true : passwordEmpty = false;
-					                  otpTF.text.replaceAll(" ", "").isEmpty ? otpEmpty = true : otpEmpty = false;
-					                });
-					                if (nameEmpty || usernameEmpty || passwordEmpty || otpEmpty) {
-					                	return;
-					                }
-									Navigator.pop(context, {
-										"name": nameTF.text, "username": usernameTF.text,
-										"password": passwordTF.text, "otp": otpTF.text,
-									});
-								},
-								icon: const Icon(Icons.add),
-							),
-						),
-					],
-				),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        title: const Text("Passwd"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              autofocus: true,
+              controller: nameTF,
+              decoration: InputDecoration(
+                labelText: "Название сервиса",
+                errorText: nameEmpty ? "Впишите название" : null,
+              ),
+            ),
+            TextField(
+              controller: usernameTF,
+              decoration: InputDecoration(
+                labelText: "Имя пользователя",
+                errorText: usernameEmpty ? "Впишите имя пользователя" : null,
+              ),
+            ),
+            TextField(
+              controller: passwordTF,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Пароль",
+                errorText: passwordEmpty ? "Впишите пароль" : null,
+              ),
+            ),
+            TextField(
+              controller: otpTF,
+              decoration: InputDecoration(
+                labelText: "Ключ",
+                errorText: otpEmpty ? "Впишите ключ OTP" : null,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    nameTF.text.replaceAll(" ", "").isEmpty
+                        ? nameEmpty = true
+                        : nameEmpty = false;
+                    usernameTF.text.replaceAll(" ", "").isEmpty
+                        ? usernameEmpty = true
+                        : usernameEmpty = false;
+                    passwordTF.text.replaceAll(" ", "").isEmpty
+                        ? passwordEmpty = true
+                        : passwordEmpty = false;
+                    otpTF.text.replaceAll(" ", "").isEmpty
+                        ? otpEmpty = true
+                        : otpEmpty = false;
+                  });
+                  if (nameEmpty || usernameEmpty || passwordEmpty || otpEmpty) {
+                    return;
+                  }
+                  Navigator.pop(context, {
+                    "name": nameTF.text,
+                    "username": usernameTF.text,
+                    "password": passwordTF.text,
+                    "otp": otpTF.text,
+                  });
+                },
+                icon: const Icon(Icons.add),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/activities/home.dart
+++ lib/activities/home.dart
@@ -1,96 +1,221 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:passwd/activities/edit.dart';
 
 class Service {
-	final String name;
-	final String username;
-	final String password;
-	final String otp;
+  final String name;
+  final String username;
+  final String password;
+  final String otp;
 
-	Service(this.name, this.username, this.password, this.otp);
+  Service(this.name, this.username, this.password, this.otp);
 }
-
 
 class Home extends StatefulWidget {
-	const Home({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
-	@override
-	HomeState createState() => HomeState();
+  @override
+  HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  List<Service> serviceList = [];
+  late AnimationController passwordTimeLineController;
 
-	List<Service> serviceList = [];
+  @override
+  void initState() {
+    passwordTimeLineController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )
+    ..addListener(() {
+      setState(() {});
+    });
+    passwordTimeLineController.repeat(reverse: true);
+    super.initState();
+  }
 
-	@override
-	void initState() {
-		super.initState();
-	}
+  @override
+  void dispose() {
+    passwordTimeLineController.dispose();
+    super.dispose();
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				shadowColor: Colors.transparent,
-				title: const Text("passwd"),
-				centerTitle: true,
-			),
-			body: ListView.builder(
-				itemCount: serviceList.length,
-				itemBuilder: (BuildContext context, int index) {
-					return Card(
-						child: Column(
-							children: [
-								ListTile(
-									title: Text(serviceList[index].name),
-									subtitle: Text(serviceList[index].username),
-								),
-								Row(
-									children: [
-										IconButton(
-											onPressed: () {
-												setState(() {
-													serviceList.removeAt(index);
-												});
-											},
-											icon: const Icon(Icons.delete),
-										),
-										IconButton(
-											onPressed: () async {
-												dynamic result = await Navigator.of(context).pushNamed(
-													"/edit",
-													arguments: EditArgs(
-														serviceList[index].name, serviceList[index].username,
-														serviceList[index].password, serviceList[index].otp,
-													),
-												);
-												setState(() {
-													serviceList[index] = (Service(result["name"], result["username"], result["password"], result["otp"]));
-												});
-											},
-											icon: const Icon(Icons.edit),
-										),
-									],
-								),
-							],
-						),
-					);
-				},
-			),
-			bottomNavigationBar: BottomAppBar(
-				shape: const CircularNotchedRectangle(),
-				child: Container(height: 50.0),
-			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: () async {
-					dynamic result = await Navigator.of(context).pushNamed("/add");
-					setState(() {
-						serviceList.add(Service(result["name"], result["username"], result["password"], result["otp"]));
-					});
-				},
-				child: const Icon(Icons.add),
-			),
-			floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        title: const Text("Passwd"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: serviceList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              showMenu(serviceList[index]);
+            },
+            child: Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(serviceList[index].name),
+                    subtitle: Text(serviceList[index].username),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        tooltip: "Удалить",
+                        onPressed: () {
+                          setState(() {
+                            serviceList.removeAt(index);
+                          });
+                        },
+                        icon: const Icon(Icons.delete),
+                      ),
+                      IconButton(
+                        tooltip: "Изменить",
+                        onPressed: () async {
+                          dynamic result =
+                              await Navigator.of(context).pushNamed(
+                            "/edit",
+                            arguments: EditArgs(
+                              serviceList[index].name,
+                              serviceList[index].username,
+                              serviceList[index].password,
+                              serviceList[index].otp,
+                            ),
+                          );
+                          setState(() {
+                            serviceList[index] = (Service(
+                                result["name"],
+                                result["username"],
+                                result["password"],
+                                result["otp"]));
+                          });
+                        },
+                        icon: const Icon(Icons.edit),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: IconTheme(
+          data: IconThemeData(color: Theme.of(context).colorScheme.primary),
+          child: Row(
+            children: [
+              IconButton(
+                tooltip: "Меню",
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Добавить",
+        onPressed: () async {
+          dynamic result = await Navigator.of(context).pushNamed("/add");
+          setState(() {
+            serviceList.add(Service(result["name"], result["username"],
+                result["password"], result["otp"]));
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  showMenu(Service service) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              topRight: Radius.circular(16.0),
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: (50 * 6).toDouble(),
+                child: Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: ListView(
+                          physics: const NeverScrollableScrollPhysics(),
+                          children: <Widget>[
+                            const Center(
+                              child: Text(
+                                "Информация",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            ListTile(
+                              title: const Text("Сервис"),
+                              subtitle: Text(service.name),
+                              onTap: () {
+                                Clipboard.setData(ClipboardData(text: service.name));
+                              },
+                            ),
+                            ListTile(
+                              title: const Text("Пользователь"),
+                              subtitle: Text(service.username),
+                              onTap: () {
+                                Clipboard.setData(ClipboardData(text: service.username));
+                              },
+                            ),
+                            ListTile(
+                              title: const Text("Пароль"),
+                              subtitle: const Text("• • • • • • • •"),
+                              onTap: () {
+                                Clipboard.setData(ClipboardData(text: service.password));
+                              },
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ListTile(
+                                    title: const Text("Ключ"),
+                                    subtitle: Text(service.otp),
+                                    onTap: () {
+                                      Clipboard.setData(ClipboardData(text: service.otp));
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(15),
+                                  child: CircularProgressIndicator(
+                                    value: passwordTimeLineController.value,
+                                    strokeWidth: 8,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
