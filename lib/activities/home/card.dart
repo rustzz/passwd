0cb007278b0634722abac136b2b5f2778c950962
rustzz <rustzz.devel@gmail.com>
2022-06-13 04:18:26 +0500

    fix: circular progress indicator

--- lib/activities/home/card.dart
+++ lib/activities/home/card.dart
@@ -6,102 +6,122 @@ class Service {
   Service(this.name, this.username, this.password, this.otp);
 }
 
-class CardInfo extends StatelessWidget {
+class CardInfo extends StatefulWidget {
   const CardInfo({
     Key? key,
     required this.serviceList,
-    required this.passwordTimeLineController,
     required this.indexOfCard,
   }) : super(key: key);
 
   final List<Service> serviceList;
-  final AnimationController passwordTimeLineController;
   final int indexOfCard;
 
   @override
+  CardInfoState createState() => CardInfoState();
+}
+
+class CardInfoState extends State<CardInfo> with TickerProviderStateMixin {
+  late AnimationController passwordTimeLineController;
+
+  @override
+  void initState() {
+    passwordTimeLineController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+    passwordTimeLineController.addListener(() {
+      setState(() {});
+    });
+    passwordTimeLineController.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    passwordTimeLineController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(16.0),
-            topRight: Radius.circular(16.0),
-          ),
-          color: Color(0xFF424242),
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
         ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: (65 * 5).toDouble(),
-              child: Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      child: ListView(
-                        physics:
-                        const NeverScrollableScrollPhysics(),
-                        children: <Widget>[
-                          const Center(
-                            child: Text(
-                              "Информация",
-                              style: TextStyle(fontSize: 20),
+        color: Color(0xFF424242),
+      ),
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: (65 * 5).toDouble(),
+            child: Stack(
+              children: [
+                Positioned(
+                  child: ListView(
+                    physics:
+                    const NeverScrollableScrollPhysics(),
+                    children: <Widget>[
+                      const Center(
+                        child: Text(
+                          "Информация",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      ListTile(
+                        title: const Text("Сервис"),
+                        subtitle:
+                        Text(widget.serviceList[widget.indexOfCard].name),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: widget.serviceList[widget.indexOfCard].name));
+                        },
+                      ),
+                      ListTile(
+                        title: const Text("Пользователь"),
+                        subtitle:
+                        Text(widget.serviceList[widget.indexOfCard].username),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: widget.serviceList[widget.indexOfCard].username));
+                        },
+                      ),
+                      ListTile(
+                        title: const Text("Пароль"),
+                        subtitle:
+                        const Text("• • • • • • • •"),
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: widget.serviceList[widget.indexOfCard].password));
+                        },
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ListTile(
+                              title: const Text("Ключ"),
+                              subtitle: Text(
+                                  widget.serviceList[widget.indexOfCard].otp),
+                              onTap: () {
+                                Clipboard.setData(ClipboardData(text: widget.serviceList[widget.indexOfCard].otp));
+                              },
                             ),
                           ),
-                          ListTile(
-                            title: const Text("Сервис"),
-                            subtitle:
-                            Text(serviceList[indexOfCard].name),
-                            onTap: () {
-                              Clipboard.setData(ClipboardData(text: serviceList[indexOfCard].name));
-                            },
-                          ),
-                          ListTile(
-                            title: const Text("Пользователь"),
-                            subtitle:
-                            Text(serviceList[indexOfCard].username),
-                            onTap: () {
-                              Clipboard.setData(ClipboardData(text: serviceList[indexOfCard].username));
-                            },
-                          ),
-                          ListTile(
-                            title: const Text("Пароль"),
-                            subtitle:
-                            const Text("• • • • • • • •"),
-                            onTap: () {
-                              Clipboard.setData(ClipboardData(text: serviceList[indexOfCard].password));
-                            },
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: ListTile(
-                                  title: const Text("Ключ"),
-                                  subtitle: Text(
-                                      serviceList[indexOfCard].otp),
-                                  onTap: () {
-                                    Clipboard.setData(ClipboardData(text: serviceList[indexOfCard].otp));
-                                  },
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(15),
-                                child: CircularProgressIndicator(
-                                  value: passwordTimeLineController.value,
-                                  strokeWidth: 8,
-                                ),
-                              ),
-                            ],
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            child: CircularProgressIndicator(
+                              value: passwordTimeLineController.value,
+                              strokeWidth: 8,
+                            ),
                           ),
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/activities/home/home.dart
+++ lib/activities/home/home.dart
@@ -10,21 +10,11 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> with TickerProviderStateMixin {
+class HomeState extends State<Home> {
   List<Service> serviceList = [];
-  late AnimationController passwordTimeLineController;
 
   @override
   void initState() {
-    passwordTimeLineController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    );
-    passwordTimeLineController.addListener(() {
-      setState(() {});
-    });
-    passwordTimeLineController.repeat(reverse: true);
-
     // for tests
     serviceList.add(Service("Youtube", "rustzz", "qwerty1", "1"));
     serviceList.add(Service("VK", "rustzz", "qwerty12", "12"));
@@ -42,12 +32,6 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
     serviceList.add(Service("Facebook", "rustzz", "qwerty123", "123456"));
     serviceList.add(Service("Reddit", "rustzz", "qwerty123", "123456"));
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    passwordTimeLineController.dispose();
-    super.dispose();
   }
 
   @override
@@ -77,7 +61,6 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
                 builder: (BuildContext context) {
                   return CardInfo(
                     serviceList: serviceList,
-                    passwordTimeLineController: passwordTimeLineController,
                     indexOfCard: index,
                   );
                 },
